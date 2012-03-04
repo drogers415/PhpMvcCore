79aaa662e265915dc5ef80d1ccbd2f27918e0712
Dennis Rogers <drogers415@gmail.com>
2012-03-04 17:42:06 -0600

    updated license info

--- mvcView.php
+++ mvcView.php
@@ -6,18 +6,18 @@
 
 	This file is part of PhpMvcCore.
 
-    MvcCore is free software: you can redistribute it and/or modify
+    PhpMvcCore is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    MvcCore is distributed in the hope that it will be useful,
+    PhpMvcCore is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+    along with PhpMvcCore.  If not, see <http://www.gnu.org/licenses/>.
 
 ************************************************************************/
 
