47a867a7beff896633db9e7f78bf3fb8f068c472
unknown <Denny@Denny-PC.(none)>
2012-03-06 19:22:08 -0600

    binding 1-dimensional arrays

--- mvcModel.php
+++ mvcModel.php
@@ -57,8 +57,60 @@ abstract class MvcBaseModelBinder {
 
 	abstract public function BindModel(&$modelState, $param, &$data=array());
 
-	protected static function FindValue($name, &$data) {
+	public static function Unbind($x) {
+		if (is_array($x)) return $x;
 
+		if (is_object($x)) {
+			$data = array();
+			$type = get_class($x);
+			$reflectClass = new ReflectionClass($type);
+			$classProperties = $reflectClass->getProperties();
+
+			foreach($classProperties as $property) {
+				$value = $property->getValue($x);
+
+				if (is_object($value))
+					continue; // not supported
+
+				$name = $property->getName();
+
+				if (is_array($value))
+					foreach($value as $k=>$v)
+						$data[$name . "[" . $k . "]"] = $v;
+				else
+					$data[$name] = $value;
+			}
+
+			return $data;
+		}
+		else
+			return array($x);
+	}
+
+	// bind value of function parameter
+	protected function BindParameter(ReflectionParameter $param, &$data) {
+		if (@$param->getClass() !== null)
+			return null; // not supported
+		else if ($param->isArray())
+			return self::BindArray($param->getName(), $data);
+		else
+			return self::BindSimpleType($param->getName(), $data);
+	}
+
+	// bind value of class property
+	protected function BindProperty($type, ReflectionProperty $property, &$data) {
+		$value = $property->getValue(new $type);
+
+		if (is_object($value))
+			return null; // not supported
+		else if (is_array($value))
+			return self::BindArray($property->getName(), $data);
+		else
+			return self::BindSimpleType($property->getName(), $data);
+	}
+
+	// bind value of simple data type (int, string, boolean)
+	private function BindSimpleType($name, &$data) {
 		// NOTE: See MvcRouteRequest definition for information about "$data."
 
 		if (isset($data[$name]))
@@ -67,10 +119,47 @@ abstract class MvcBaseModelBinder {
 			return $_GET[$name];
 		elseif (isset($_POST[$name]))
 			return $_POST[$name];
-		elseif (count($data))
+		elseif (count($data) && array_values($data) === $data)
 			return array_shift($data);
 		else
 			return null;
+	}
+
+	// bind 1-dimensional array
+	private function BindArray($name, &$data) {
+		$callback = create_function('$key', 'return strpos($key, "' . $name . '[") === 0;');
+
+		$array = null;
+		if (count($array = self::FilterArrayKeys($data, $callback)) < 1)
+			if (count($array = self::FilterArrayKeys($_GET, $callback)) < 1)
+				if (count($array = self::FilterArrayKeys($_POST, $callback)) < 1)
+					return null;
+
+		foreach($array as $key=>$value) {
+			unset($array[$key]);
+			$index = strtok($key, "[");
+			$index = strtok("]");
+			$array[$index] = $value;
+		}
+
+		return $array;
+	}
+
+	private function FilterArrayKeys($array, $callback) {
+		if (!is_array($array)) {
+			trigger_error( 'array_filter_key() expects parameter 1 to be array, ' . gettype($array) . ' given', E_USER_WARNING );
+			return null;
+		}
+		
+		if (empty($array)) return $array;
+		
+		$array = array_flip($array);
+		$array = array_filter($array,$callback);
+		if (empty($array)) return array();
+		
+		$array = array_flip($array);
+
+		return $array;
 	}
 }
 
@@ -79,13 +168,13 @@ class MvcDefaultModelBinder extends MvcBaseModelBinder {
 	public function BindModel(&$modelState, $param, &$data=array()) {
 		$model = null;
 
-		if(@$param->getClass() == null)
-			$model = $this->FindValue($param->getName(), $data);
-		else {
+		if (@$param->getClass() !== null) {
 			$model = $this->BindClassModel($param->getClass()->name, $data);
 			if ($model instanceof IMvcModelValidator)
 				$modelState = $this->ValidateClassModel($model);
 		}
+		else
+			$model = $this->BindParameter($param, $data);
 
 		return $model;
 	}
@@ -97,7 +186,8 @@ class MvcDefaultModelBinder extends MvcBaseModelBinder {
 		$classProperties = $reflectClass->getProperties();
 
 		foreach($classProperties as $classProperty) {
-			$value = $this->FindValue($classProperty->getName(), $data);
+			$value = $this->BindProperty($type, $classProperty, $data);
+
 			if ($value !== null) {
 				if ($model == null) $model = new $type;
 				$classProperty->setValue($model, $value);
